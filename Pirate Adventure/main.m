20591be9fba6a6c254b07ec104232318cba633bb
Ximo <Ximo@Mac-Pro-de-Ximo.local>
2014-07-07 16:41:27 +0200

    Initial Commit

---
+++ Pirate Adventure/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Pirate Adventure
+//
+//  Created by Ximo on 07/07/14.
+//  Copyright (c) 2014 Ximo Alloza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XAAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XAAppDelegate class]));
+    }
+}
