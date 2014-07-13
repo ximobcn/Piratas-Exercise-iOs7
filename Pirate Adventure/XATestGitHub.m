820a233f0687ab73cc11694763b82e0d70efe485
Ximo <Ximo@Mac-Pro-de-Ximo.local>
2014-07-13 12:50:53 +0200

    added a test class

---
+++ Pirate Adventure/XATestGitHub.m
@@ -0,0 +1,13 @@
+//
+//  XATestGitHub.m
+//  Pirate Adventure
+//
+//  Created by Ximo on 13/07/14.
+//  Copyright (c) 2014 Ximo Alloza. All rights reserved.
+//
+
+#import "XATestGitHub.h"
+
+@implementation XATestGitHub
+
+@end
